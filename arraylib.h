622243cd51c3f58d580324aa3294bd749836b52c
ya576 <yashkumarsoni8256@gmail.com>
2025-10-20 10:55:25 +0530

    Create arraylib.h

---
+++ arraylib.h
@@ -0,0 +1,7 @@
+int findMaxIndex(int arr[], int size);
+int findMinIndex(int arr[], int size);
+float findAverage(int arr[], int size);
+void displayArray(int arr[], int size);
+void reverseArray(int arr[], int size);
+void sortArray(int arr[], int size);
+int linearSearch(int arr[], int size, int value);
